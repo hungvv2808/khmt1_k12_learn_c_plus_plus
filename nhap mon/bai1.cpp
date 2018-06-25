4915715cc2d11707820f482cdbe6b82744986dee
kingkong <vuhung2881999@gmail.com>
2018-06-25 12:12:28 +0700

    Nhap mon C++

---
+++ nhap mon/bai1.cpp
@@ -0,0 +1,10 @@
+//xuat gia tri
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout <<"hello,My name is Boss. "<< endl;
+  return 0;
+}
---
+++ nhap mon/bai2.cpp
@@ -0,0 +1,12 @@
+//tinh gia tri
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int x=3;
+  float y=2.3;
+  cout<<"Gia tri can tim la:  "<< x+y <<endl;
+  return 0;
+}
